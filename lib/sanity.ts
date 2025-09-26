8ba376d2255aa6fecbd0a08d239a81775bd2691e
AfgPress <AfgPress@proton.me>
2025-09-26 03:17:10 -0400

    Updated sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,6 +2,8 @@
  import { createClient } from 'next-sanity'
 
  export const client = createClient({
+ }
+ )
 -  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'uiu9mgqs',
 +  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'uiu9mgqs',
    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
