5b2287b5c6c8fb7daf13a1e923e54e537a909e84
AfgPress <AfgPress@proton.me>
2025-09-29 00:21:54 -0400

    Updated sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -8,6 +8,7 @@
     console.log('Fetched posts from Sanity:', posts);
     return posts;
   projectId: 'uiu9mgqs',
+ )
 +  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'uiu9mgqs',
    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
    apiVersion: '2024-01-01',
